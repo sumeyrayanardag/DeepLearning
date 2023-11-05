f462bf9b01ab7630db4c1f7344689a64c22f0872
Sümeyra YANARDAĞ <45365584+sumeyrayanardag@users.noreply.github.com>
2023-11-05 14:17:15 +0300

    Add guessing game

---
+++ 1_GuessingGame.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+import random
+
+
+# In[24]:
+
+
+print("Welcome to the guessing game! You will have a maximum of 5 attempts to guess the correct number.")
+
+
+# In[18]:
+
+
+number = random.randint(1, 100)
+
+
+# In[20]:
+
+
+username = input("Please enter your name: ")
+
+
+# In[23]:
+
+
+flag = 0
+for x in range(5):
+    guess = int(input("Please enter your %d. guess: " %(x+1)))
+    if number == guess:
+        print("Congratulations " + username + "! The number is %d" %number)
+        flag = 1
+        break
+    elif number - guess > 0:
+        print("Too low!")
+    else:
+        print("Too high!")
+        
+if(flag == 0):
+    print("You lost " + username + "! The number is %d" %number)
+
+
+# In[ ]:
+
+
+input("If you want to exit, please press the enter")
+
